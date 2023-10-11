39a252cd1c39751537df804ece76a5527950a1a0
DESKTOP-BV59EF7\Acer-7 <josipcestar2@gmail.com>
2023-10-12 00:50:20 +0200

    fix

--- WebAppii/Controllers/HoodieController.cs
+++ WebAppii/Controllers/HoodieController.cs
@@ -10,22 +10,24 @@ using WebAppii.Models;
 using WebAppii.Service;
 using WebAppii.Service.Common;
 using WebApiPractice.Common;
+using Autofac.Core;
+
 namespace WebAppii.Controllers
 {
     public class HoodieController : ApiController
     {
 
-        private IHoodieService service;
+        private IHoodieService _service;
 
-        public HoodieController(IHoodieService _service)
+        public HoodieController(IHoodieService service)
         {
-            service = _service;
+            _service = service;
         }
         
         [HttpPost]
         public async Task<HttpResponseMessage>  Add(Hoodie hoodie)
         {
-            string acc = await service.Post(hoodie);
+            string acc = await _service.Post(hoodie);
             if (acc == "Created")
             {
                 return Request.CreateResponse(HttpStatusCode.OK, "Added");
@@ -35,11 +37,28 @@ namespace WebAppii.Controllers
             return Request.CreateResponse(HttpStatusCode.InternalServerError, "Not added");
            
         }
+
+        [HttpDelete]
+        public async Task<HttpResponseMessage> Delete(Guid id)
+        {
+            string acc = await _service.Delete(id);
+            if (acc == "Deleted")
+            {
+                return 
+                    Request.CreateResponse(HttpStatusCode.OK, "Deleted");
+            }
+
+            return Request.CreateResponse(HttpStatusCode.InternalServerError, "Not deleted");
+
+
+
+        }
+
         [HttpGet]
 
         public async Task<HttpResponseMessage> Get(Guid id)
         {
-            Hoodie hoodie =await service.GetHoodieById(id);
+            Hoodie hoodie =await _service.GetHoodieById(id);
             if (hoodie != null)
             {
                 return Request.CreateResponse(HttpStatusCode.OK, hoodie);
@@ -58,7 +77,7 @@ namespace WebAppii.Controllers
             Paging paging = new Paging(pageSize,pageNumber);
             Sorting sorting = new Sorting(sortBy,sortOrder);
             Filtering filtering = new Filtering(querryName,sortSize,sortStye);
-            List<Hoodie> hoodies = await service.GetAll(paging,sorting,filtering);
+            List<Hoodie> hoodies = await _service.GetAll(paging,sorting,filtering);
             if (hoodies != null)
             {
                 return Request.CreateResponse(HttpStatusCode.OK, hoodies);
@@ -76,7 +95,7 @@ namespace WebAppii.Controllers
             
         {
            
-           string acc = await service.Update(hoodie, id);
+           string acc = await _service.Update(hoodie, id);
            if (acc == "Updated")
            {
                     return Request.CreateResponse(HttpStatusCode.OK, "Updated");
@@ -87,28 +106,6 @@ namespace WebAppii.Controllers
             }
         }
 
-        [HttpDelete]
-        public async Task<HttpResponseMessage> Delete(Guid id)
-        {
-            string acc = await service.Delete(id);
-            if (acc == "Deleted")
-            {
-                return Request.CreateResponse(HttpStatusCode.OK, "Deleted");
-            }
-            else if (acc == "Hoodie not found")
-            {
-                return Request.CreateResponse(HttpStatusCode.NotFound, "Hoodie not found");
-            }
-            else if (acc == "Error")
-            {
-                return Request.CreateResponse(HttpStatusCode.InternalServerError, "Error");
-            }
-            else
-            {
-                return Request.CreateResponse(HttpStatusCode.InternalServerError, "Not deleted");
-            }
-        }
 
-        
-    }
 }
+
